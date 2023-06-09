329b36e492e9ab4029bda7ecc81ebd53d860b5b9
Thomas Szentirmay <tlszent@outlook.com>
2023-06-08 21:50:24 -0400

    got stuck on timer bug so asked ChatGPT for assistance, and re-did JS code with the recommended changes

--- assets/script.js
+++ assets/script.js
@@ -85,7 +85,6 @@ var questions = [
     },
 ];
 
-// FETCH ELEMENTS WITH QUERYSELECTOR TO USE
 var startBtn = document.getElementById("start");
 var messageContainer = document.getElementById("message-container");
 var quizContainer = document.getElementById("quiz-container");
@@ -95,46 +94,30 @@ var questionElement = document.getElementById("question");
 var choicesElement = document.getElementById("choices");
 var resultElement = document.getElementById("result");
 var currentQuestionIndex = 0;
-
-// START GAME WITH START BUTTON
+var timer = 90;
 
 function startGame() {
-    // HIDE MESSAGE SECTION
     messageContainer.style.display = "none";
-
-    // SHOW QUIZ SECTION
     document.querySelector("main").style.display = "flex";
-
-    var timeRemaining = 60; // INITIAL TIME IN SECONDS
-
-    timerElement.textContent = timeRemaining;
-
-    // START COUNTDOWN TIMER
+    timerElement.textContent = timer;
     countdown = setInterval(function () {
-        timeRemaining--;
-
-        if (timeRemaining >= 0) {
-            timerElement.textContent = timeRemaining;
+        timer--;
+        if (timer >= 0) {
+            timerElement.textContent = timer;
         } else {
             clearInterval(countdown);
             console.log("Time's up!");
         }
     }, 1000);
-
-    displayQuestion(); // Display the first question immediately
+    displayQuestion();
 }
 
 startBtn.addEventListener("click", startGame);
 
-
 function displayQuestion() {
-    var currentQuestion = questions[currentQuestionIndex]; // Get the current question object
-    questionElement.textContent = currentQuestion.question; // Display the question
-
-    // Clear the choices element
+    var currentQuestion = questions[currentQuestionIndex];
+    questionElement.textContent = currentQuestion.question;
     choicesElement.innerHTML = "";
-
-    // Create and append choice buttons
     for (var i = 0; i < currentQuestion.options.length; i++) {
         var choice = currentQuestion.options[i];
         var li = document.createElement("li");
@@ -144,43 +127,30 @@ function displayQuestion() {
         li.appendChild(button);
         choicesElement.appendChild(li);
     }
-
-    // Attach event listener to the choices element using event delegation
     choicesElement.addEventListener("click", checkAnswer);
 }
 
 function checkAnswer(event) {
-    var selectedChoice = event.target.textContent; // Get the text content of the clicked button
-    var currentQuestion = questions[currentQuestionIndex]; // Get the current question object
-
+    var selectedChoice = event.target.textContent;
+    var currentQuestion = questions[currentQuestionIndex];
     if (selectedChoice === currentQuestion.answer) {
-        resultElement.textContent = "Correct!"; // Display correct message
+        resultElement.textContent = "Correct!";
+        setTimeout(function () {
+            resultElement.textContent = "";
+            currentQuestionIndex++;
+            if (currentQuestionIndex < questions.length) {
+                displayQuestion();
+            } else {
+                console.log("Quiz completed!");
+            }
+        }, 1000);
     } else {
-        resultElement.textContent = "Wrong!"; // Display wrong message
-        // Decrease the timer by 5 seconds only if the answer is incorrect
-        if (timer > 5) {
-            timer -= 5;
+        resultElement.textContent = "Wrong!";
+        if (timer > 10) {
+            timer -= 10;
         } else {
-            timer = 0; // Set the timer to 0 if it goes below 5 seconds
+            timer = 0;
         }
+        timerElement.textContent = timer;
     }
-
-    // Update the timer element with the new value
-    timerElement.textContent = timer;
-
-    // Clear the result after 2 seconds
-    setTimeout(function () {
-        resultElement.textContent = "";
-    }, 2000);
-
-    // Move to the next question after 2 seconds
-    setTimeout(function () {
-        currentQuestionIndex++;
-        if (currentQuestionIndex < questions.length) {
-            displayQuestion();
-        } else {
-            // Handle quiz completion here (e.g., show final score, submit form, etc.)
-            console.log("Quiz completed!");
-        }
-    }, 2000);
 }
