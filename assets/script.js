84560280b976d50929c4237febc01408ff22bf1f
Thomas Szentirmay <tlszent@outlook.com>
2023-06-08 21:33:05 -0400

    questions from question array have been loaded into the game, but now experiencing bugs with the timer, so trying to fix them

--- assets/script.js
+++ assets/script.js
@@ -3,85 +3,85 @@ var questions = [
     {
         question: "What is JavaScript?",
         options: [
-            "A. A programming language used to make websites interactive",
-            "B. A markup language for creating website structures",
-            "C. A style sheet language used for website design",
-            "D. A database management system",
+            "A programming language used to make websites interactive",
+            "A markup language for creating website structures",
+            "A style sheet language used for website design",
+            "A database management system",
         ],
-        answer: "A",
+        answer: "A programming language used to make websites interactive",
     },
     {
         question: "Which keyword is used to declare a variable in JavaScript?",
-        options: ["A. let", "B. variable", "C. var", "D. const"],
-        answer: "C",
+        options: ["let", "variable", "var", "const"],
+        answer: "var",
     },
     {
         question: 'What is the result of 3 + "7" in JavaScript?',
-        options: ["A. 10", "B. 37", "C. 21", "D. NaN"],
-        answer: "B",
+        options: ["10", "37", "21", "NaN"],
+        answer: "37",
     },
     {
         question: "Which built-in method is used to convert a string to lowercase?",
         options: [
-            "A. toLowerCase()",
-            "B. toLower()",
-            'C. changeCase("lower")',
-            "D. lowerCase()",
+            "toLowerCase()",
+            "toLower()",
+            'changeCase("lower")',
+            "lowerCase()",
         ],
-        answer: "A",
+        answer: "toLowerCase()",
     },
     {
         question: 'What is the purpose of the "document.getElementById()" method?',
         options: [
-            "A. To retrieve an HTML element based on its class",
-            "B. To retrieve an HTML element based on its ID",
-            "C. To retrieve an HTML element based on its tag name",
-            "D. To retrieve an HTML element based on its value",
+            "To retrieve an HTML element based on its class",
+            "To retrieve an HTML element based on its ID",
+            "To retrieve an HTML element based on its tag name",
+            "To retrieve an HTML element based on its value",
         ],
-        answer: "B",
+        answer: "To retrieve an HTML element based on its ID",
     },
     {
         question: 'What does the "=== operator" do in JavaScript?',
         options: [
-            "A. Checks for value equality without type conversion",
-            "B. Assigns a value to a variable",
-            "C. Performs arithmetic addition",
-            "D. Compares two variables",
+            "Checks for value equality without type conversion",
+            "Assigns a value to a variable",
+            "Performs arithmetic addition",
+            "Compares two variables",
         ],
-        answer: "A",
+        answer: "Checks for value equality without type conversion",
     },
     {
         question:
             "Which array method adds new elements to the end of an array and returns the new length?",
-        options: ["A. push()", "B. concat()", "C. slice()", "D. pop()"],
-        answer: "A",
+        options: ["push()", "concat()", "slice()", "pop()"],
+        answer: "push()",
     },
     {
         question:
             'What is the purpose of the "addEventListener()" method in JavaScript?',
         options: [
-            "A. To add a new CSS class to an HTML element",
-            "B. To remove an HTML element from the DOM",
-            "C. To attach an event handler function to an HTML element",
-            "D. To change the text content of an HTML element",
+            "To add a new CSS class to an HTML element",
+            "To remove an HTML element from the DOM",
+            "To attach an event handler function to an HTML element",
+            "To change the text content of an HTML element",
         ],
-        answer: "C",
+        answer: "To attach an event handler function to an HTML element",
     },
     {
         question:
             "What is the correct way to write a single-line comment in JavaScript?",
         options: [
-            "A. // This is a comment",
-            "B. <!-- This is a comment -->",
-            "C. * This is a comment *",
-            "D. # This is a comment",
+            "// This is a comment",
+            "<!-- This is a comment -->",
+            "* This is a comment *",
+            "# This is a comment",
         ],
-        answer: "A",
+        answer: "// This is a comment",
     },
     {
         question: 'What does the "typeof" operator return for an array?',
-        options: ["A. object", "B. array", "C. string", "D. boolean"],
-        answer: "A",
+        options: ["object", "array", "string", "boolean"],
+        answer: "object",
     },
 ];
 
@@ -91,6 +91,10 @@ var messageContainer = document.getElementById("message-container");
 var quizContainer = document.getElementById("quiz-container");
 var timerElement = document.getElementById("timer");
 var countdown;
+var questionElement = document.getElementById("question");
+var choicesElement = document.getElementById("choices");
+var resultElement = document.getElementById("result");
+var currentQuestionIndex = 0;
 
 // START GAME WITH START BUTTON
 
@@ -115,8 +119,68 @@ function startGame() {
             clearInterval(countdown);
             console.log("Time's up!");
         }
-    }, 1000); 
+    }, 1000);
+
+    displayQuestion(); // Display the first question immediately
 }
 
 startBtn.addEventListener("click", startGame);
 
+
+function displayQuestion() {
+    var currentQuestion = questions[currentQuestionIndex]; // Get the current question object
+    questionElement.textContent = currentQuestion.question; // Display the question
+
+    // Clear the choices element
+    choicesElement.innerHTML = "";
+
+    // Create and append choice buttons
+    for (var i = 0; i < currentQuestion.options.length; i++) {
+        var choice = currentQuestion.options[i];
+        var li = document.createElement("li");
+        var button = document.createElement("button");
+        button.classList.add("choice");
+        button.textContent = choice;
+        li.appendChild(button);
+        choicesElement.appendChild(li);
+    }
+
+    // Attach event listener to the choices element using event delegation
+    choicesElement.addEventListener("click", checkAnswer);
+}
+
+function checkAnswer(event) {
+    var selectedChoice = event.target.textContent; // Get the text content of the clicked button
+    var currentQuestion = questions[currentQuestionIndex]; // Get the current question object
+
+    if (selectedChoice === currentQuestion.answer) {
+        resultElement.textContent = "Correct!"; // Display correct message
+    } else {
+        resultElement.textContent = "Wrong!"; // Display wrong message
+        // Decrease the timer by 5 seconds only if the answer is incorrect
+        if (timer > 5) {
+            timer -= 5;
+        } else {
+            timer = 0; // Set the timer to 0 if it goes below 5 seconds
+        }
+    }
+
+    // Update the timer element with the new value
+    timerElement.textContent = timer;
+
+    // Clear the result after 2 seconds
+    setTimeout(function () {
+        resultElement.textContent = "";
+    }, 2000);
+
+    // Move to the next question after 2 seconds
+    setTimeout(function () {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length) {
+            displayQuestion();
+        } else {
+            // Handle quiz completion here (e.g., show final score, submit form, etc.)
+            console.log("Quiz completed!");
+        }
+    }, 2000);
+}
